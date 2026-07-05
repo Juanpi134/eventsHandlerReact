595d86405353d12d8828f606f04e40a426e7ea1d
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-05 09:59:13 -0300

    se añaden comentarios para el input de valor numerico

--- src/componentes/MyComponent.jsx
+++ src/componentes/MyComponent.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function MiComponente(){
     const [nombre,setNombre] = useState("");
+    const [cantidad,setCantidad] = useState(0);
 
     //me muestra undefined
     //console.log(nombre);
@@ -11,9 +12,18 @@ function MiComponente(){
         setNombre(evento.target.value);
     }
 
+    function manejarCambioDeCantidad(evento){
+        //cada vez que invoco esto se puede ir aumentando de 1 en 1 
+       setCantidad(evento.target.value)
+    }
+
     return (
         <div>
-            <input type="text"  value={name} onChange={manejarCambioDeNombre} />
+            <input type="text"  value={nombre} onChange={manejarCambioDeNombre} />
+            <p>{nombre}</p>
+
+            {/* si le saco el valor tanto al nombre como la cantidad no me deja escribir */}
+            <input type="number" value={cantidad}  onChange={manejarCambioDeCantidad}/>
         </div>
     )
 }
